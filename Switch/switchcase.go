15da4e2309de8fb1d8ff84062d19650820c85562
MBKenn <teerapat.yj@gmail.com>
2023-06-08 13:18:46 +0700

    Update Switch

---
+++ Switch/switchcase.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+	var input string
+	//var colorCode string
+	fmt.Scan(&input)
+	switch input {
+	case "blue":
+		fmt.Println("#0000FF")
+	case "green":
+		fmt.Println("#008000")
+	case "pink":
+		fmt.Println("#FFC0CB")
+	case "yellow":
+		fmt.Println("#FFFF00")
+	default:
+		fmt.Println("none")
+	}
+}
+
+func num(){
+	input := 2
+	switch input {
+	case 1:
+		fmt.Println("one")
+	case 2:
+		fmt.Println("two")
+	case 3:
+		fmt.Println("three")
+	default:
+		fmt.Println("none")
+	}
+}
